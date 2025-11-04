636e596bdc3e130883de4fee766d0ad7c6c1c4ab
NauvalTazzaky <nauvaltazzaky04@gmail.com>
2025-11-03 19:21:44 -1200

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,1 +1,18 @@
-<?php // Kode untuk koneksi ke database agar program terkoneksi ?>
+<?php
+// Konfigurasi Database (Ganti sesuai setting Anda)
+$server = "localhost";
+$username = "root";
+$password = "your_password"; // Ganti dengan password database Anda
+$database = "aplikasi_data"; // Ganti dengan nama database Anda
+
+// Melakukan koneksi
+$koneksi = mysqli_connect($server, $username, $password, $database);
+
+// Cek koneksi
+if (!$koneksi) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+
+// Opsional: pesan sukses
+// echo "Koneksi database berhasil!";
+?>
